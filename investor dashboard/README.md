697db04f9b9fdaade764c3cb6a73890ba70d6295
Maame Lankar De Veer <maamelankar.deveer@gmail.com>
2022-08-05 04:05:22 +0000

    dashboard with html css js

---
+++ investor dashboard/README.md
@@ -0,0 +1,3 @@
+Responsive Investment Dashboard With A Functioning Day/night Mode created using HTML CSS And Javascript and the tutorial by <a href="https://www.youtube.com/c/EGATORTUTORIALS">EGATOR</a>
+Tutorial Link <a href="https://youtu.be/FaBY9yAUtdg">here</a>
+![Project Thumbnail](./thumbnail.png)
---
+++ investor dashboard/js/index.js
@@ -0,0 +1,52 @@
+const chart = document.querySelector('#chart').getContext('2d');
+
+// Create a new chart instance
+new Chart(chart, {
+    type: 'line',
+    data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov'],
+
+        datasets: [
+            {
+                label: 'BTC',
+                data: [29374, 33537, 49631, 59095, 57828, 36684, 33527, 39974, 48116, 61004],
+                borderColor: 'red',
+                borderWidth: 2
+            },
+            {
+                label: 'ETH',
+                data: [31500, 11000, 88800, 26000, 46000, 32698, 5000, 3000, 38116, 41004],
+                borderColor: 'blue',
+                borderWidth: 2
+            }
+        ]
+    },
+    Options: {
+        responsive: true
+    }
+}) 
+
+// show or hide sidebar
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const sidebar = document.querySelector('aside');
+
+// show sidebar
+menuBtn.addEventListener('click', () => {
+    sidebar.style.display = 'block';
+})
+
+// close sidebar
+closeBtn.addEventListener('click', () => {
+    sidebar.style.display = 'none';
+})
+
+// change theme
+const themeBtn = document.querySelector('.theme-btn');
+
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+
+    themeBtn.querySelector('span:first-child').classList.toggle('active');
+    themeBtn.querySelector('span:last-child').classList.toggle('active');
+});
